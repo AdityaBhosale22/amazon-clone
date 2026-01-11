25eec78e8dae718f049991e07c6479982a58c575
Aditya Bhosale <adityabhosale4388@gmail.com>
2026-01-11 22:55:13 +0530

    practice continued

--- transition/script.js
+++ transition/script.js
@@ -82,74 +82,211 @@ const classroom = [
 //     }
 // }
 
-class StudentDetails {
-    constructor(name, rollNo, age, gender){
-        this.name = name;
-        this.rollNo = rollNo;
-        this.age = age;
-        this.gender = gender;
-    }
-    aboutStudent(){
-        return `Name: ${this.name}, Roll No: ${this.rollNo}, Age: ${this.age}, Gender: ${this.gender}`;
-    }
+// class StudentDetails {
+//     constructor(name, rollNo, age, gender){
+//         this.name = name;
+//         this.rollNo = rollNo;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+//     aboutStudent(){
+//         return `Name: ${this.name}, Roll No: ${this.rollNo}, Age: ${this.age}, Gender: ${this.gender}`;
+//     }
 
-    changeName(newName){
-        this.name = newName;
-        console.log("Name changed to: " + this.name);
-    }
-}
+//     changeName(newName){
+//         this.name = newName;
+//         console.log("Name changed to: " + this.name);
+//     }
+// }
 
-let myName= "Eleven";
-let myRollNo= "11";
-let myAge = "18";
-let myGender = "female"
+// let myName= "Eleven";
+// let myRollNo= "11";
+// let myAge = "18";
+// let myGender = "female"
 
-let student1 = new StudentDetails("oliver", '01', "17", "male");
-// let student2 = new StudentDetails(myName, myRollNo, myAge, myGender);
-// console.log(student2);
+// let student1 = new StudentDetails("oliver", '01', "17", "male");
+// // let student2 = new StudentDetails(myName, myRollNo, myAge, myGender);
+// // console.log(student2);
 
-// console.log(student1.aboutStudent());
-// student1.changeName("Lucas");
-// console.log(student1);
+// // console.log(student1.aboutStudent());
+// // student1.changeName("Lucas");
+// // console.log(student1);
 
-class Person {
-    constructor(name, age, gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-    speak(){
-        return "Speaks Native Language";
-    }
+// class Person {
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+//     speak(){
+//         return "Speaks Native Language";
+//     }
 
-    eat(food){
-        return "Eats food " + food;
-    }
-}
+//     eat(food){
+//         return "Eats food " + food;
+//     }
+// }
 
-class Teacher extends Person{
-    constructor(name, age, gender, company){
-        super(name, age, gender);
-        this.company = company;
-    }
-    speak(){
-        return "Speaks English and Hindi";
-    }
-    // eat(food){
-    //     return "Eats only"+" "+ food;
-    // }
+// class Teacher extends Person{
+//     constructor(name, age, gender, company){
+//         super(name, age, gender);
+//         this.company = company;
+//     }
+//     speak(){
+//         return "Speaks English and Hindi";
+//     }
+//     // eat(food){
+//     //     return "Eats only"+" "+ food;
+//     // }
 
-    speakAtHome(){
-        return super.speak();
-    }
-}
-let person1 = new Person("Erica", 15, "female");
-let teacher1 = new Teacher("Mr. Smith", 40, "male", "Hawkins High School");
+//     speakAtHome(){
+//         return super.speak();
+//     }
+// }
+// let person1 = new Person("Erica", 15, "female");
+// let teacher1 = new Teacher("Mr. Smith", 40, "male", "Hawkins High School");
 // console.log(teacher1);
 // console.log(teacher1.speak());
 // console.log(person1);
 // console.log(person1.speak());
 
-console.log(teacher1.eat("pizza and coke"))
+// console.log(teacher1.eat("pizza and coke"))
 
-console.log(teacher1.speakAtHome())
+// console.log(teacher1.speakAtHome())
+
+class User{
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    get info(){
+        return `Username: ${this.username}, Email: ${this.email}`;
+    }
+    set updateEmail(newEmail){
+        this.email = newEmail;
+    }
+}
+
+let user1 = new User("WillByers", "willbyers@example.com", "password123");
+// console.log(user1.info);
+// user1.updateEmail = "willbyers_new@example.com";
+// console.log(user1.info);
+
+
+
+class Person {
+	constructor(name, age){
+		this._name = name; 
+        this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(updatedAge){
+		this._age = updatedAge;
+	}
+}
+
+class Student extends Person {
+	constructor(name, age){
+		super(name, age);
+	}
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age){
+		super(name, age);
+	}
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
+
+// const person = new Person("John", 25);
+// console.log(person.name);
+// person.age = 26;
+// console.log(person._age);
+
+// const student = new Student("Mike", 20);
+// student.study();
+
+// const teacher = new Teacher("Sarah", 35);
+// teacher.teach();
+
+class Animal {
+	constructor(species){
+		this._species = species;
+	}
+	get species(){
+		return this._species;
+	}
+    makeSound(){
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
+
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof")
+	}
+}
+
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr")
+	}
+}
+
+const myCat = new Cat("Siamese");
+// myCat.makeSound();
+// myCat.purr();
+
+const myDog = new Dog("Husky");
+// myDog.makeSound();
+// myDog.bark();
+
+class Rectangle {
+	constructor(width, height){
+		this._width = width;
+		this._height = height;
+	}
+	get width(){
+		return this._width;
+	}
+	get height(){
+		return this._height;
+	}
+	getArea(){
+		return `${this.width*this.height}`
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side){
+		super(side, side);
+	}
+	getPerimeter(){
+		return `${4*this.width}`
+	}
+	
+}
+// const square = new Square(7);
+// console.log(square.width); // Output: 7
+// console.log(square.height); // Output: 7
+// console.log(square.getArea()); // Output: 49
+// console.log(square.getPerimeter()); // Output: 28
+
+// const rectangle = new Rectangle(5, 10);
+// console.log(rectangle.width); // Output: 5
+// console.log(rectangle.height); // Output: 10
+// console.log(rectangle.getArea()); // Output: 50

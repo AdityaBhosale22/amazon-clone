13650f1f64cbd4c29ddeadd8ed1bb2859d2abacf
Aditya Bhosale <adityabhosale4388@gmail.com>
2026-01-12 23:00:40 +0530

    add fixes

--- transition/script.js
+++ transition/script.js
@@ -280,13 +280,13 @@ class Square extends Rectangle {
 	}
 	
 }
-// const square = new Square(7);
-// console.log(square.width); // Output: 7
-// console.log(square.height); // Output: 7
-// console.log(square.getArea()); // Output: 49
-// console.log(square.getPerimeter()); // Output: 28
-
-// const rectangle = new Rectangle(5, 10);
-// console.log(rectangle.width); // Output: 5
-// console.log(rectangle.height); // Output: 10
-// console.log(rectangle.getArea()); // Output: 50
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
+console.log("-----");
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50

4ee43c5ac45f388d23153589e84ae06c487c9ece
Aditya Bhosale <adityabhosale4388@gmail.com>
2026-01-17 23:29:10 +0530

    more dom added

--- dom/script.js
+++ dom/script.js
@@ -3,3 +3,21 @@ b.addEventListener("click", function() {
     document.getElementById("heading").innerText = "Magic Happened!";
     document.body.style.backgroundColor = "lightblue";
 });
+
+let p = document.getElementById("hoverParagraph");
+p.addEventListener("mouseover", function() {
+    p.style.fontWeight = "bold";
+});
+p.addEventListener("mouseout", function() {
+    p.style.fontWeight = "normal";
+});
+
+let i = document.getElementById("textInput");
+i.addEventListener("input", function() {
+    document.getElementById("displayText").innerText = i.value;
+});
+
+let f = document.getElementById("backgroundChange");
+f.addEventListener("change", function() {
+    document.body.style.backgroundColor = f.value;
+});
